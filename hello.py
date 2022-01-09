940e47f90cb46245d1642bcb3784316d2aba1222
zohansyahfatomi <zohansyahfatomi@gmail.com>
2022-01-10 01:39:25 +0700

    variable rules

--- hello.py
+++ hello.py
@@ -8,7 +8,36 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!!!1!</p>"
 
+#route
 @app.route("/<name>")
 def hello(name):
     return f"Hello, {escape(name)}!"
+
+#variable rules
+@app.route('/user/<username>')
+def show_user_profile(username):
+    #show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    #show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
+'''
+string : (default) accepts any text without slash
+int : accepts positive integers
+float : accepts positive floating point values 
+path : like string but also accepts slashes
+uuid : accepts UUID strings
+'''
+
+
+
+
+
 
